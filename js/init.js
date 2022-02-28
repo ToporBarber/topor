4a68df9195c0fb3493455d97a9c0e97a293740f8
ToporBarber <99681972+ToporBarber@users.noreply.github.com>
2022-03-01 03:31:38 +0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -303,19 +303,19 @@ function topor_tm_contact_form(){
 		}
 	});
 	
-	$(".topor_tm_button").click(function(){
-		var name 		= jQuery(".contact_form #name").val();
-		var email 		= jQuery(".contact_form #email").val();
-		var message 	= jQuery(".contact_form #message").val();
-		var phone 	= jQuery(".contact_form #phone").val();
+// 	$(".topor_tm_button").click(function(){
+// 		var name 		= jQuery(".contact_form #name").val();
+// 		var email 		= jQuery(".contact_form #email").val();
+// 		var message 	= jQuery(".contact_form #message").val();
+// 		var phone 	= jQuery(".contact_form #phone").val();
 		
-		if(name===''||email===''||phone===''||message===''){
-			jQuery('div.empty_notice').slideDown(500).delay(2000).slideUp(500);
-		}
-		else if (emailTest(email)){
-			jQuery('div.email_notice').slideDown(500).delay(2000).slideUp(500);
-		}
-	});
+// 		if(name===''||email===''||phone===''||message===''){
+// 			jQuery('div.empty_notice').slideDown(500).delay(2000).slideUp(500);
+// 		}
+// 		else if (emailTest(email)){
+// 			jQuery('div.email_notice').slideDown(500).delay(2000).slideUp(500);
+// 		}
+// 	});
 	
 	jQuery(".contact_form #send_message").on('click', function(){
 		
